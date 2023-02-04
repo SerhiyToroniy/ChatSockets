c8afb093c887286c8027fa86d20dda073fc40b87
SerhiyToroniy <toroniyserhiy@gmail.com>
2023-02-04 15:14:52 +0200

    Add project files.

---
+++ ChatSockets/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ChatSockets.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ChatSockets/wwwroot/js/site.js
@@ -0,0 +1,40 @@
+﻿$(function () {
+    var protocol = location.protocol === "https:" ? "wss:" : "ws:";
+    var wsUri = protocol + "//" + window.location.host;
+    var socket = new WebSocket(wsUri);
+    socket.onopen = e => {
+        console.log("socket opened", e);
+    };
+
+    socket.onclose = function (e) {
+        console.log("socket closed", e);
+    };
+
+    socket.onmessage = function (e) {
+        console.log(e);
+        const p = document.createElement("p");
+        p.setAttribute("class", "one-msg")
+        const node = document.createTextNode(`${e.data}`);
+        p.appendChild(node);
+
+        const element = document.getElementById("msgs");
+        element.appendChild(p);
+    };
+
+    socket.onerror = function (e) {
+        console.error(e.data);
+    };
+
+    $('#MessageField').keypress(function (e) {
+        if (e.which != 13) {
+            return;
+        }
+
+        e.preventDefault();
+        var userName = document.getElementById("UsernameField").value;
+        console.log(userName);
+        var message = userName + ": " + $('#MessageField').val();
+        socket.send(message);
+        $('#MessageField').val('');
+    });
+});
